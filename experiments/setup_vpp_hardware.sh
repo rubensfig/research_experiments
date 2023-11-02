b8aa1b9eb558b50199e4eacd266a0481e722da3e
Rubens Figueiredo <r.figueiredo.52@gmail.com>
2023-11-02 11:18:34 +0100

    fix paths

--- experiments/setup_vpp_hardware.sh
+++ experiments/setup_vpp_hardware.sh
@@ -5,11 +5,11 @@ RETRIES=10
 LABEL="vpp-rate"
 
 for i in "${RANGE[@]}"; do
-    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/hw_loadgen_setup.yml -e stream2_rate=$i
+    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/hw_loadgen_vpp_setup.yml -e stream2_rate=$i
 
     for j in $(seq 1 $RETRIES); do
         echo $i $j
-        ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/hw_loadgen_test.yml -e measure_id=$j -e range=$i -e label=$LABEL
+        ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/hw_loadgen_vpp_test.yml -e measure_id=$j -e range=$i -e label=$LABEL
      done
 
     ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml vpp_teardown.yml
