aa2835d90a75b4cc7a0560e029ce361a8fcf82e6
Ubuntu <ubuntu@testing.labshared.bisdn.de>
2023-11-10 13:55:09 +0000

    tofino-flows-25gbps: add points

--- experiments/setup_tofino_trex.sh
+++ experiments/setup_tofino_trex.sh
@@ -2,7 +2,7 @@
 
 RANGE=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
 RETRIES=1
-LABEL="tofino-flows"
+LABEL="tofino-flows-25gbps"
 
 for i in "${RANGE[@]}"; do
     ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_tofino_setup.yml
