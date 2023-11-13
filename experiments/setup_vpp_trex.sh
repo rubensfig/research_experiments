ba7af37eea17b8d13d880505059c9729de4d8c67
Rubens Figueiredo <r.figueiredo.52@gmail.com>
2023-11-13 21:04:16 +0100

    vpp-flow-rate: add files

--- experiments/setup_vpp_trex.sh
+++ experiments/setup_vpp_trex.sh
@@ -2,14 +2,17 @@
 
 RANGE=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
 RETRIES=10
-LABEL="vpp-flows-1gbps"
+MULT="1gbpsl1"
+LABEL="vpp-flow-${MULT}"
 
 for i in "${RANGE[@]}"; do
     for j in $(seq 1 $RETRIES); do
-        echo $i $j
-        ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e stream2_rate=$i
-        ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_test.yml -e measure_id=$j -e range=$i -e label=$LABEL
-        ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml
+        for k in 64 256 512 1500; do
+            echo $i $j $k;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e packet_size=$k;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_test.yml -e measure_id=$j -e range=$i -e label=$LABEL -e packe_size=$k;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml;
+        done;
      done
 done
 
