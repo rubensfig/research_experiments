9405f40e365383d9f7ce6553c1196523abdd2b89
Ubuntu <ubuntu@ubuntu@>
2023-11-29 08:17:25 +0000

    1gbpsl1-1500: add points

--- experiments/setup_vpp_trex.sh
+++ experiments/setup_vpp_trex.sh
@@ -2,12 +2,12 @@
 
 RANGE=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096)
 RETRIES=10
-MULT="2gbpsl1"
+MULT="1gbpsl1"
 LABEL="vpp-flow-${MULT}"
 
 for i in "${RANGE[@]}"; do
     for j in $(seq 1 $RETRIES); do
-        for k in 64 256 512 1500; do
+        for k in 1500; do
             echo $i $j $k;
             ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e packet_size=$k;
             ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_test.yml -e measure_id=$j -e range=$i -e label=$LABEL -e packet_size=$k;
