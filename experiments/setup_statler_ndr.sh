ce84f602a80e932b6512e72593be2126f9c13a2c
Your Name <you@example.com>
2024-02-06 10:19:03 +0000

    l3fwd-1th-hqos-6_1mbps-4flow: add points

--- experiments/setup_statler_ndr.sh
+++ experiments/setup_statler_ndr.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-RANGE=(1)
+RANGE=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096)
+# RANGE=(1)
 RETRIES=3
-FREQ=3000
 
-LABEL="ndr-bypass-2thread_non_sibling-ht"
+LABEL="l3fwd-1th-no_membuf-max-ndr"
 for i in "${RANGE[@]}"; do
-    for j in $(seq 1 $RETRIES); do
-	for k in 64 256 512 1500; do
+    for measure in $(seq 1 $RETRIES); do
+	# for k in 64 256 512 1500; do
+	for k in 64; do
 	    echo $i $j $k;
-	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e packet_size=$k;
-	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_ndr_vpp_test.yml -e measure_id=$j -e range="L3FWD" -e label=$LABEL -e packet_size=$k -e mult=$load;
-	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml;
+	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e measure_id=${measure} -e range=${i} -e label=$LABEL -e packet_size=$k -e mult=$load;
+	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_ndr_vpp_test.yml -e measure_id=${measure} -e range=${i} -e label=$LABEL -e packet_size=$k -e mult=$load;
+	    ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml -e measure_id=${measure} -e range=${i} -e label=$LABEL -e packet_size=$k -e mult=$load;
 	done;
      done
 done
--- experiments/setup_vpp_trex.sh
+++ experiments/setup_vpp_trex.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-RANGE=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096)
-RETRIES=10
-MULT="3gbpsl1"
-LABEL="vpp-flow-${MULT}"
+RANGE=(1 64 128 256 512 1024 2048 4096)
+RETRIES=3
+MULT="30mpps"
+LABEL="l3fwd-4th-hqos-6.1mbps"
 
 for i in "${RANGE[@]}"; do
-    for j in $(seq 1 $RETRIES); do
-        for k in 1500; do
+    for measure in $(seq 1 $RETRIES); do
+        for k in 64 256 512 1500; do
             echo $i $j $k;
-            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e packet_size=$k;
-            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_test.yml -e measure_id=$j -e range=$i -e label=$LABEL -e packet_size=$k;
-            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_setup.yml -e measure_id=${measure} -e range=${i} -e label=$LABEL -e packet_size=$k -e mult=$MULT;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/trex_loadgen_vpp_test.yml -e measure_id=${measure} -e range=$i -e label=$LABEL -e packet_size=$k -e mult=$MULT;
+            ansible-playbook -i ../inventories/labshared-setup.bisdn.de.yaml ../ansible/vpp_teardown.yml -e measure_id=${measure} -e range=${i} -e label=$LABEL -e packet_size=$k -e mult=$MULT;
         done;
      done
 done
-
